bb994c3e7a36472a9e19cb3d79c948b0b0b69041
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-07-01 12:26:06 +0200

    add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 
+import "./App.css";
+import "./lib/font-awesome/css/all.min.css";
+
+import Add from "./components/Add";
+import Header from "./components/Header";
+import Watched from "./components/Watched";
+import Watchlist from "./components/Watchlist";
+
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Watchlist />} />
+        <Route path="/watched" element={<Watched />} />
+        <Route path="/add" element={<Add />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="container">
+      <div className="inner-content">
+        <div className="brand">
+          <Link to="/">WatchList</Link>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Watch List</Link>
+          </li>
+          <li>
+            <Link to="/watched">Watched</Link>
+          </li>
+          <li>
+            <Link to="/add" className="btn">
+              +Add
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
