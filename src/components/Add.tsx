480dc7106ac1abb21f38db4fb472b147abcbc021
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-07-01 12:29:42 +0200

    connect to TMDB movie API

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
 
 const Add = () => {
+  const [query, setQuery] = useState<string>("");
+  const [results, setResults] = useState<any[]>([]);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&query=${e.target.value}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.errors) setResults(data.results);
+        else setResults([]);
+      });
+  };
+
   return (
     <div className="add-page">
       <div className="container">
         <div className="add-content">
           <div className="input-wrapper">
-            <input type="text" placeholder="Search for a movie" />
+            <input
+              type="text"
+              placeholder="Search for a movie"
+              value={query}
+              onChange={onChange}
+            />
           </div>
         </div>
       </div>
